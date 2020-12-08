c04e6af0e9f54447db652725fecbbb5dc7beb4a3
k-Ryunosuke <s19C1030UK@s.chibakoudai.jp>
2020-12-08 11:51:09 +0000

    関数修正

--- myled.c
+++ myled.c
@@ -20,87 +20,62 @@ static volatile u32 *gpio_base = NULL;
 static int led_array[4] = {25,4,12,16};
 
 
+static void led_on(void){
+        gpio_base[7] = 1 << 25;
+        mdelay(100);
+        gpio_base[10] = 1 << 25;
+        mdelay(100);
+        gpio_base[7] = 1 << 12;
+        mdelay(100);
+        gpio_base[10] = 1 << 12;
+        mdelay(100);
+        gpio_base[7] = 1 << 4;
+        mdelay(100);
+        gpio_base[10] = 1 << 4;
+        mdelay(100);
+        gpio_base[7] = 1 << 16;
+        mdelay(100);
+        gpio_base[10] = 1 << 16;
+        mdelay(100);
+}
+
+static void all_off(void){
+        gpio_base[7] = 1 << 25;
+        gpio_base[7] = 1 << 12;
+        gpio_base[7] = 1 << 4;
+        gpio_base[7] = 1 << 16;
+}
+
+static void all_on(void){
+        gpio_base[10] = 1 << 25;
+        gpio_base[10] = 1 << 12;
+        gpio_base[10] = 1 << 4;
+        gpio_base[10] = 1 << 16;
+}
+
+
 static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_t* pos){
         char c;
         int i;
+
         if(copy_from_user(&c,buf,sizeof(char))) return -EFAULT;
+
         if(c == '0'){
                 gpio_base[10] = 1 <<25;
         }else if(c=='1'){
-                gpio_base[7] = 1 << 25;
-                mdelay(100);
-                gpio_base[10] = 1 << 25;
-                mdelay(100);
-                gpio_base[7] = 1 << 12;
-                mdelay(100);
-                gpio_base[10] = 1 << 12;
-                mdelay(100);
-                gpio_base[7] = 1 << 4;
-                mdelay(100);
-                gpio_base[10] = 1 << 4;
-                mdelay(100);
-                gpio_base[7] = 1 << 16;
-                mdelay(100);
-                gpio_base[10] = 1 << 16;
-                mdelay(100);
+                led_on();
 
         }else if(c=='2'){
                 for(i=0;i<2;i++){
-                        gpio_base[7] = 1 << 25;
-                        mdelay(100);
-                        gpio_base[10] = 1 << 25;
-                        mdelay(100);
-                        gpio_base[7] = 1 << 12;
-                        mdelay(100);
-                       gpio_base[10] = 1 << 12;
-                        mdelay(100);
-                        gpio_base[7] = 1 << 4;
-                        mdelay(100);
-                        gpio_base[10] = 1 << 4;
-                        mdelay(100);
-                        gpio_base[7] = 1 << 16;
-                        mdelay(100);
-                        gpio_base[10] = 1 << 16;
-                        mdelay(100);
-
+                        led_on();
                 }
         }else if(c=='3'){
                 for(i=0;i<3;i++){
-                        gpio_base[7] = 1 << 25;
-                        mdelay(100);
-                        gpio_base[10] = 1 << 25;
-                        mdelay(100);
-                        gpio_base[7] = 1 << 12;
-                        mdelay(100);
-                        gpio_base[10] = 1 << 12;
-                        mdelay(100);
-                        gpio_base[7] = 1 << 4;
-                        mdelay(100);
-                        gpio_base[10] = 1 << 4;
-                        mdelay(100);
-                        gpio_base[7] = 1 << 16;
-                        mdelay(100);
-                        gpio_base[10] = 1 << 16;
-                        mdelay(100);
+                        led_on();
                 }
         }else if(c=='4'){
                 for(i=0;i<4;i++){
-                        gpio_base[7] = 1 << 25;
-                        mdelay(100);
-                        gpio_base[10] = 1 << 25;
-                        mdelay(100);
-                        gpio_base[7] = 1 << 12;
-                        mdelay(100);
-                        gpio_base[10] = 1 << 12;
-                        mdelay(100);
-                        gpio_base[7] = 1 << 4;
-                        mdelay(100);
-                        gpio_base[10] = 1 << 4;
-                        mdelay(100);
-                        gpio_base[7] = 1 << 16;
-                        mdelay(100);
-                        gpio_base[10] = 1 << 16;
-                        mdelay(100);
+                        led_on();
                 }
 
         }else if(c == '5'){
@@ -130,14 +105,7 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
                         gpio_base[10] = 1 << 12;
                         mdelay(200-40*i);
                 }
-                gpio_base[7] = 1 << 25;
-                mdelay(50);
-                gpio_base[7] = 1 << 12;
-                mdelay(50);
-                gpio_base[7] = 1 << 4;
-                mdelay(50);
-                gpio_base[7] = 1 << 16;
-                mdelay(100);
+                all_off();
                 for(i=0;i<15;i++){
                         gpio_base[10] = 1 << 25;
                         gpio_base[10] = 1 << 4;
@@ -150,34 +118,17 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
                         gpio_base[7] = 1 << 4;
                         mdelay(50);
                 }
-                gpio_base[10] = 1 << 12;
-                gpio_base[10] = 1 << 16;
-                gpio_base[10] = 1 << 4;
-                gpio_base[10] = 1 << 25;
+                all_on();
                 for(i=0;i<15;i++){
-                        gpio_base[7] = 1 << 12;
-                        gpio_base[7] = 1 << 4;
-                        gpio_base[7] = 1 << 25;
-                        gpio_base[7] = 1 << 16;
+                        all_off();
                         mdelay(30);
-                        gpio_base[10] = 1 << 12;
-                        gpio_base[10] = 1 << 4;
-                        gpio_base[10] = 1 << 25;
-                        gpio_base[10] = 1 << 16;
+                        all_on();
                         mdelay(100);
                 }
                 mdelay(1000);
-                gpio_base[7] = 1 << 12;
-                gpio_base[7] = 1 << 16;
-                gpio_base[7] = 1 << 4;
-                gpio_base[7] = 1 << 25;
+                all_off();
                 mdelay(3000);
-                gpio_base[10] = 1 << 12;
-                gpio_base[10] = 1 << 16;
-                gpio_base[10] = 1 << 4;
-                gpio_base[10] = 1 << 25;
-
-
+                all_on();
         }
         return 1;
 }
@@ -185,7 +136,6 @@ static ssize_t led_write(struct file* filp, const char* buf, size_t count, loff_
 static struct file_operations led_fops ={
         .owner = THIS_MODULE,
         .write = led_write,
-        //.read = sushi_read
 };
 
 static int __init init_mod(void){
@@ -198,15 +148,19 @@ static int __init init_mod(void){
         printk(KERN_INFO "%s is loaded major:%d\n", __FILE__, MAJOR(dev));
         cdev_init(&cdv, &led_fops);
         retval = cdev_add(&cdv, dev, 1);
+
         if(retval < 0){
-               printk(KERN_ERR "cdev_add failed. major:%d.\n minor:%d.\n",MAJOR(dev),MINOR(dev));
+                printk(KERN_ERR "cdev_add failed. major:%d.\n minor:%d.\n",MAJOR(dev),MINOR(dev));
                 return retval;
         }
+
         cls = class_create(THIS_MODULE,"myled");
+
         if(IS_ERR(cls)){
                 printk(KERN_ERR"class_create failed");
                 return PTR_ERR(cls);
         }
+
         device_create(cls, NULL, dev,NULL, "myled%d",MINOR(dev));
 
         gpio_base = ioremap_nocache(0x3f200000,0xA0);
@@ -218,7 +172,6 @@ static int __init init_mod(void){
         const u32 mask = ~(0x7 << shift);
         gpio_base[index] = (gpio_base[index] & mask) | (0x1 << shift);
         }
-
         return 0;
 }
 
@@ -227,7 +180,6 @@ static void __exit cleanup_mod(void){
         device_destroy(cls,dev);
         class_destroy(cls);
         unregister_chrdev_region(dev,1);
-        //printk(KERN_INFO"%s is unloaded.major:%d\n",__FILE__,MAJOR(dev));
 }
 
 module_init(init_mod);
